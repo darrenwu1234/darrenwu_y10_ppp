b798175cf65c4c6276ed122162bf54375e58a01f
darrenwu4321 <darrenwu@connect.kellettschool.com>
2025-04-03 05:45:49 +0000

    Apr 3, 2025, 5:45 AM

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from collections import Counter
 from random import randint
-
+value_dict = {"A" : 1, "B": 3, "C": 3, "D" : 2, "E": 1, "F": 4,"G": 2,"H": 4, "I": 1, "J": 8, "K": 5, "L": 1,"M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10}
+board = []
+for i in range(15):
+    
+    board.append([])
+for i in range(len(board)):
+    for f in range(15):
+        board[i].append([])
+for i in range(len(board)):
+    print(board)
 def random_tile():
         global tile_list
         return tile_list[randint(1,len(tile_list)-1)]
@@ -85,6 +94,7 @@ def show_info():
     print(" ")
 def show_move():
     print(origin, destination)
+
 
     
     
