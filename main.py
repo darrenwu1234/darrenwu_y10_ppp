6846487fb2eb9a092ba7e774183e30a485188332
darrenwu4321 <darrenwu@connect.kellettschool.com>
2025-06-03 03:05:39 +0000

    Jun 3, 2025, 3:05 AM

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def show_board():
     
 def show_move():
     player_tiles[player_turn]
-    tile = player_tiles[player_turn][int(origin)-1]
+    tile = player_tiles[player_turn][int(origin)]
     print(f"Tile {tile} moved from {origin} to {destination.upper()}.")
     return tile
 
