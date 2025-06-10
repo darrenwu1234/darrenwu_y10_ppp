c170f1a734a3bf51e51b77ec562c7c6e808bf308
darrenwu4321 <darrenwu@connect.kellettschool.com>
2025-06-10 03:41:35 +0000

    Jun 10, 2025, 3:41 AM

--- main.py
+++ main.py
@@ -103,11 +103,14 @@ def get_destination(move_type):
                 print("Not a valid position, please enter a letter A-P followed by a number 1-16 e.g(D13)")
                 origin = input("Enter the position where you want to draw your tile from: ")
     else:
-        if destination not in ["1","2","3","4","5","6","7"]:
+        while valid_move == False:
+            if destination not in ["1","2","3","4","5","6","7"]:
 
-            print("Not a valid position, please input a number")
+                print("Not a valid position, please input a number")
 
-            destination = input("Please enter where you want to place your tile")
+                destination = input("Please enter where you want to place your tile")
+            else:
+                valid_move = True
         
     return destination
 global player_turn
