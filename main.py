652fd94da59c3ee085b6c20fe4030c3b888a59e0
darrenwu4321 <darrenwu@connect.kellettschool.com>
2025-06-12 14:45:37 +0000

    Jun 12, 2025, 2:45 PM

--- main.py
+++ main.py
@@ -216,8 +216,10 @@ def is_valid_output(temp_board):
     valid_output = False
     for i in temp_board:
         if valid_output == False:
-            for (f,g) in zip(temp_board,range(1,len(temp_board))):
+            g=0
+            for f in temp_board:
                 print("b")
+                g+=1
                 print(i,f,g)
                 if i != f:
                     print("a")
