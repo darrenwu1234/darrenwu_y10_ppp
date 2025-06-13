c8672b7a556cc34b73045a8054cba0acc25ef29d
darrenwu4321 <darrenwu@connect.kellettschool.com>
2025-06-13 04:36:12 +0000

    Jun 13, 2025, 4:36 AM

--- main.py
+++ main.py
@@ -214,11 +214,17 @@ def is_valid_output(temp_board):
         
         for i in temp_board:
             word += i[0]
+    else:
+        print("Word is not valid")
+
     if word in dictionary_set:
         valid_output = True
+        print("Word is valid")
     else:
         valid_output = False
-    print(valid_output)
+        print("Word is not valid")
+    
+    
     
 def is_straight(temp_board):
     valid_output = True
