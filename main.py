2c8e0f5796360b6ef79c6c13004df71eb249a349
darrenwu4321 <darrenwu@connect.kellettschool.com>
2025-06-13 01:45:07 +0000

    Jun 13, 2025, 1:45 AM

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from random import randint
-dictionary_set = set(open('dictionary.txt'))
+dictionary_set = set(line.strip() for line in open('dictionary.txt'))
 print(dictionary_set)
 value_dict = {"A" : 1, "B": 3, "C": 3, "D" : 2, "E": 1, "F": 4,"G": 2,"H": 4, "I": 1, "J": 8, "K": 5, "L": 1,"M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10}
 board = []
